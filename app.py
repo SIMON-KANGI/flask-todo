614ed5a28f8bbc64d38f03ee96611d6be1c52241
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-03-28 18:57:07 +0300

    Ft:comments

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect ##import the libraries
+from flask_sqlalchemy import SQLAlchemy ## make sure to install flask_sqlalchemy
 from datetime import datetime
-from flask_migrate import Migrate
+from flask_migrate import Migrate # get the migration files for updating the database when needed
 
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-class Todo(db.Model):
+app = Flask(__name__) # initialize the application
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' #configure the databsabase using these commands
+db = SQLAlchemy(app) # create a database instance
+migrate = Migrate(app, db) # helps to update the database schema
+class Todo(db.Model): # create a CLASS for todo
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(255), nullable=False)      # set the database structure /ORM
     description = db.Column(db.String(255), nullable=False)
     status = db.Column(db.String(20), nullable=False, default='Pending')  # Add status field
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
+    def __repr__(self): # representation
         return '<Todo %r>' % self.title
-
-@app.route('/', methods=['POST','GET'])
+#app decorator for creating the filesystems
+@app.route('/', methods=['POST','GET']) #CALL POST request and GET request METHODS
 def home():
     if request.method == "POST":
         task = request.form['task']
@@ -27,8 +27,8 @@ def home():
         status = request.form['status']
         todo = Todo(title=task, description=description, status=status)
         try:
-            db.session.add(todo)
-            db.session.commit()
+            db.session.add(todo)# add task to database
+            db.session.commit() # COMMIT WORK
             return redirect('/')
         except Exception as e:
             print(e)
@@ -38,7 +38,7 @@ def home():
         return render_template('home.html', tasks=tasks)
 
 
-@app.route('/delete/<int:id>')
+@app.route('/delete/<int:id>') #delete a task
 def delete(id):
     task_delete = Todo.query.get_or_404(id)
     
@@ -49,8 +49,8 @@ def delete(id):
     except Exception as e:
         print(e)
         return redirect('/')
-    
-@app.route('/update/<int:id>', methods=['POST', 'GET'])
+ # update a task   
+@app.route('/update/<int:id>', methods=['POST', 'GET']) # call the post and get methods
 def update(id):
     task = Todo.query.get_or_404(id)
     
@@ -69,6 +69,6 @@ def update(id):
     
     
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+    with app.app_context(): # create a context for the application
+        db.create_all() #this will run the database when the app is run
     app.run(debug=True)
